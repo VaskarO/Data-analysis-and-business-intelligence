33661963431fa316d1ea611ba694faf3fc2ab642
VaskarO <contact2vo@gmail.com>
2024-08-16 21:47:15 +0200

    Equity research QA LLM app

--- LLM apps/Equity research LLM app/main.py
+++ LLM apps/Equity research LLM app/main.py
@@ -5,6 +5,7 @@ import langchain
 from langchain import OpenAI
 from langchain.document_loaders import UnstructuredURLLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 import streamlit as st
@@ -45,3 +46,19 @@ if process_url_clicked:
         pickle.dump(vectorstore_openai, f)
 
 query = main_placeholder.text_input("Question: ")
+
+if query:
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            result = chain({"question": query}, return_only_outputs=True)
+            st.header("Answer")
+            st.write(result["answer"])
+
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")
+                for source in sources_list:
+                    st.write(source)
